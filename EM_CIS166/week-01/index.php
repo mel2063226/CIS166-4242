5d63b89dd4a4e4b19a91c732975559097e27c7c1
mel2063226 <135192158+mel2063226@users.noreply.github.com>
2024-01-29 21:39:48 -0700

    Update index.php
    Little editing

--- EM_CIS166/week-01/index.php
+++ EM_CIS166/week-01/index.php
@@ -1,20 +1,25 @@
-ExampleGet your own PHP Server
 <!DOCTYPE html>
 <html>
 <body>
 
 <?php 
-echo "<h1> Module 2 Assignment" //An page heading (H1)
-echo "Mindy Benson" //Using 'echo' or 'print' display your name
+//An page heading (H1)
+echo "<h1> Module 2 Assignment<br>"; 
+//Using 'echo' or 'print' display your name
+echo "Mindy Benson<br>"; 
 //Display the following dates (one per line)
-echo "Today is " . date("Y/m/d") . "<br>"; //Todays date
-echo "The time is " . date("h:i:sa") . "<br>"; //Current time
-echo "In one week it will be " . date('Y/m/d h:i:s', strtotime('+7 days', time())) . "<br>"; //The date/time one week in future
-$date1 = strtotime('2023/1/1'); //How many days since 1/1/2023
+//Todays date
+echo "Today is " . date("Y/m/d") . "<br>"; 
+//Current time
+echo "The time is " . date("h:i:sa") . "<br>"; 
+//The date/time one week in future
+echo "In one week it will be " . date('Y/m/d h:i:s', strtotime('+7 days', time())) . "<br>"; 
+//How many days since 1/1/2023
+$date1 = strtotime('2023/1/1'); 
 $date2 = strtotime('2024/1/29');
 $diff = $date2 - $date1;
 $days = floor($diff / (60 * 60 * 24));
-echo $days;
+echo $days. "<br>";
 
 //How many weeks until this spring's graduation. 
 function week_between_two_dates($date1, $date2)
@@ -31,4 +36,3 @@ echo 'There are '. week_between_two_dates($dt1, $dt2). " weeks between now ($dt1
 ?>
 </body>
 </html>
-
