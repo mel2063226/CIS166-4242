07384df93cd08c0ab66607d65088f72ba11dc1f2
mel2063226 <135192158+mel2063226@users.noreply.github.com>
2024-02-08 17:23:25 -0700

    Week 2 Work

---
+++ week-2/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--Mindy Benson-->
+
+<html>
+  <head>
+    <title>Assignemnt 2 Form</title>
+  </head>
+  <body>
+    <form action="processing.php" method="get">
+      <table>
+        <tr>
+          <td>First Name:</td>
+          <td><input type="text" name="firstName" /></td>
+        </tr>
+        <tr>
+          <td>Last Name:</td>
+          <td><input type="text" name="lastName" /></td>
+        </tr>
+        <tr>
+          <td>Email Address:</td>
+          <td><input type="email" name="email" /></td>
+        </tr>
+        <tr>
+          <td>Comment:</td>
+          <td><input type="text" name="txtComment" /></td>
+        </tr>
+        <tr>
+         <td colspan="2"><input type="submit" value="Submit the form" /></td>
+        </tr>
+      </table>
+    </form>
+  </body>
+</html>
