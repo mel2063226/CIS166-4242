da64dbb23ca945898698086c63caa88676389e5f
CIS166ae <122632455+CIS166ae@users.noreply.github.com>
2024-03-19 18:04:13 -0700

    Update index-scott.php

--- week-7/index-scott.php
+++ week-7/index-scott.php
@@ -1,1 +1,44 @@
+<?php
+  // Mindy Benson
+  // 3/18/2024
+  // CIS166AE Module 8
+  
+  // Include the LoginBox class definition.
+  require_once 'loginBox.php';
+  
+  // Start the session. (not needed yet, but will soon.  it belongs here)
+  session_start();
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+  <h1>Login</h1>
+
+  <?php
+    // Initialize LoginBox object
+    $loginBox = new LoginBox();
+
+    // Check if form is submitted.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      // Retrieve the form data.
+      $username = $_POST["username"] ?? NULL;
+      $password = $_POST["password"] ?? NULL;
+       
+      // Authenticate function will redirect user to correct page.
+      $loginBox->authenticate($username, $password);
+    }
+    else {
+      // Displays the login form using LoginBox object.
+      $loginForm = $loginBox->getLoginForm();
+      echo $loginForm;
+    }
+  ?>
+
+</body>
+</html>
